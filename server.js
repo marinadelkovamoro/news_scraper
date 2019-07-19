55f251536998925c7a00bdae6953cbc9b0fbe26d
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-18 19:27:51 -0700

    established mongoose connection to the database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -6,7 +8,17 @@ const app = express();
 const router = express.Router();
 
 app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
 app.use(router);
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/sandiegotheatreHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+
 
 app.listen(PORT, function () {
     console.log("App running and server listening on port:" + PORT);

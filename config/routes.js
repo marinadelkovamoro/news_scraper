bb3bb145f7993f9f88f201ed37eda55c8a98335e
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-18 22:09:37 -0700

    added routes to routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,9 @@
+const scrape = require("../scripts/scrape");
+
+const headlinesController = require("../controllers/headlines");
+const notesController = require("../controllers/notes");
+
+
 module.exports = function (router) {
     router.get("/", (req, res) => {
         res.render("home");
@@ -6,5 +12,69 @@ module.exports = function (router) {
 
     router.get("/saved", (req, res) => {
         res.render("saved");
+    });
+
+    router.get("api/fetch", (req, res) => {
+        headlinesController.fetch((err, docs) => {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new headlines"
+                });
+            } else {
+                res.json({
+                    message: docs.insertedCount + "new headlines added"
+                });
+            }
+        });
+    });
+
+    router.get("/api/headlines", (req, res) => {
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
+
+        headlinesController.get(query, function (data) {
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/headlines/:id", (req, res) => {
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function (err, data) {
+            res.json(data);
+        });
+    });
+
+    router.patch("/api/headlines", (req, res) => {
+        headlinesController.update(req.body, function (err, data) {
+            res.json(data);
+        });
+    });
+
+    router.get("/api/notes/:headline_id?", (req, res) => {
+        var query = {};
+        if (req.params.healine_id) {
+            query._id = req.params.healine_id;
+        }
+
+        notesController.get(query, function (err, data) {
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/notes/:id", (req, res) => {
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data) {
+            res.json(data);
+        });
+    });
+
+    router.post("/api/notes", (req, res) => {
+        notesController.save(req.body, function(data) {
+            res.json(data);
+        });
     });
 }
--- controllers/headlines.js
+++ controllers/headlines.js
@@ -4,15 +4,15 @@ const Headline = require("../models/Headline");
 
 module.exports = {
     fetch: function (callback) {
-        scrape((data) => {
+        scrape(function (data) {
             var headlines = data;
             for (var i = 0; i < headlines.lenght; i++) {
                 headlines[i].saved = false;
             }
-        });
-        // mongo syntax for inserting many headlines without stopping the code even if there is an error in some of the data that gets scraped; the code will skip over this data 
-        Headline.collection.insertMany(headlines, function (error, response) {
-            callback(error, response);
+            // mongo syntax for inserting many headlines without stopping the code even if there is an error in some of the data that gets scraped; the code will skip over this data 
+            Headline.collection.insertMany(headlines, function (error, response) {
+                callback(error, response);
+            });
         });
     },
     delete: function (query, callback) {
@@ -24,8 +24,8 @@ module.exports = {
     },
     // updates new headlliness with relevant id
     // updates any information that is passed with these new headlines
-    update: function(query, callback) {
-        Headline.update({_id: query._id}, {
+    update: function (query, callback) {
+        Headline.update({ _id: query._id }, {
             $set: query
         }, {}, callback);
     }
